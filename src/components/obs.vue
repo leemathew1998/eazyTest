fe43e59b515d806678fa5784fad3222903b4f5c5
魏加锴 <jkwei@foxmail.com>
2022-10-22 18:01:23 +0800

    temp

--- src/components/obs.vue
+++ src/components/obs.vue
@@ -1,29 +1,18 @@
 <template>
-  <div class="video">
-    <video id="videoElement"></video>
-  </div>
+  <div class="video">123456</div>
 </template>
 
 <script setup>
-import flvjs from 'flv.js/dist/flv.js';
 // rtmp://192.168.3.10:1935/live/test
-import {ref,reactive} from 'vue'
-if(flvjs.isSupported()){
-    var videoElement = document.getElementById('videoElement');
-    var flvPlayer = flvjs.createPlayer({
-        type: 'video/rtmp',
-        "isLive": true,
-        url: 'rtmp://192.168.3.10:1935/live/test'
-    });
-    flvPlayer.attachMediaElement(videoElement);
-    flvPlayer.load();
-    flvPlayer.play();
-}
+import { ref, reactive } from "vue";
+import { AliPlayerV3 } from "vue-aliplayer-v3";
+console.log(AliPlayerV3);
 </script>
 
 <style scoped lang="less">
 .video {
   width: 100%;
+  height: 50%;
   padding: 20px;
   box-sizing: border-box;
 }

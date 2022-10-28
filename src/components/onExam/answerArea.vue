04fc5106128bbba8f37d1c99aa23ee1765a408b0
魏加锴 <jkwei@foxmail.com>
2022-10-28 11:04:37 +0800

    qwdef

--- src/components/onExam/answerArea.vue
+++ src/components/onExam/answerArea.vue
@@ -12,6 +12,7 @@
           <div
             v-for="(item, index) in questions"
             :key="`${item.type}-${item.count}`"
+            :class="[`${item.type}-${item.count}`, `${item.type}`]"
           >
             <div class="item-title">
               <span class="item-title-count">{{ item.count }}、</span>
@@ -23,18 +24,18 @@
             </div>
           </div>
           <!-- 一直没有解决的高度问题 -->
-          <div class="h-12"></div>
+          <div class="h-12 mark"></div>
         </div>
       </div>
     </template>
   </BlankCardWithoutIcon>
 </template>
 <script setup>
-import { ref } from "vue";
+import { onMounted, ref } from "vue";
 import { questions } from "./constants.js";
 import BlankCardWithoutIcon from "./blankCardWithoutIcon.vue";
 import { Radio, CheckBox, WriteDown, Judge, Coding } from "./optionModules";
-
+import lodash from "lodash";
 const showTitle = ref("单选");
 const stringMapInstance = {
   单选: Radio,
@@ -43,10 +44,58 @@ const stringMapInstance = {
   判断: Judge,
   编程: Coding,
 };
+const mapEl = [];
+const indexMapToTitle = ["单选", "多选", "简答", "判断", "编程"];
+function handleScroll() {
+  const nowScrollTop =
+    document.getElementsByClassName("answer-container")[0].scrollTop;
+  for (let index = 0; index < mapEl.length; index++) {
+    if (nowScrollTop < mapEl[index + 1] && nowScrollTop > mapEl[index]) {
+      showTitle.value = indexMapToTitle[index];
+      break;
+    }
+  }
+}
+onMounted(() => {
+  /*
+     *@Author: jkwei
+     *@Date: 2022-10-28 10:13:26
+     *@Description: 初步想法是在此处获取到每种题目单个的高度，然后监听滚动高度，看看是不是到达？
+     假设每种题目20道
+    */
+  const radioHeight = document.getElementsByClassName("单选")[0].offsetHeight; //单个题目高度
+  const checkBoxHeight =
+    document.getElementsByClassName("多选")[0].offsetHeight; //单个题目高度
+  const JudgeHeight = document.getElementsByClassName("判断")[0].offsetHeight; //单个题目高度
+  const writeDownHeight =
+    document.getElementsByClassName("简答")[0].offsetHeight; //单个题目高度
+  const codingHeight = document.getElementsByClassName("编程")[0].offsetHeight; //单个题目高度
+  const el = document.getElementsByClassName("answer-container")[0];
+  mapEl.push(0);
+  mapEl.push(radioHeight * 20);
+  mapEl.push(radioHeight * 20 + checkBoxHeight * 20);
+  mapEl.push(radioHeight * 20 + checkBoxHeight * 20 + JudgeHeight * 20);
+  mapEl.push(
+    radioHeight * 20 +
+      checkBoxHeight * 20 +
+      JudgeHeight * 20 +
+      writeDownHeight * 20
+  );
+  mapEl.push(
+    radioHeight * 20 +
+      checkBoxHeight * 20 +
+      JudgeHeight * 20 +
+      writeDownHeight * 20 +
+      codingHeight * 20
+  );
+  //举个例子：[0, 3040, 7360, 9120, 11900, 16180] 16227
+  el.addEventListener("scroll", lodash.throttle(handleScroll, 200), false);
+});
 </script>
 <style lang="less" scoped>
 .answer-container {
   min-height: 60vh;
+  overflow-y: scroll;
   max-height: 80vh;
   .item-title {
     display: flex;
--- src/components/onExam/constants.js
+++ src/components/onExam/constants.js
@@ -17,20 +17,20 @@ for (let index = 20; index < 40; index++) {
 }
 for (let index = 40; index < 60; index++) {
   questions.push({
-    type: "简答",
+    type: "判断",
     count: index + 1,
     content: "下列哪一项属性不属于字体<font>标记属性。",
   });
 }
 for (let index = 60; index < 80; index++) {
   questions.push({
-    type: "判断",
+    type: "简答",
     count: index + 1,
     content: "下列哪一项属性不属于字体<font>标记属性。",
   });
 }
 
-for (let index = 80; index < 85; index++) {
+for (let index = 80; index < 100; index++) {
   questions.push({
     type: "编程",
     count: index + 1,

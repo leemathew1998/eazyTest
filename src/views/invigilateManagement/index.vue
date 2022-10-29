8a76cc0efd2bfeb9039a3f2df6f3d5f330adc3a2
魏加锴 <jkwei@foxmail.com>
2022-10-29 09:58:59 +0800

    x

--- src/views/invigilateManagement/index.vue
+++ src/views/invigilateManagement/index.vue
@@ -10,7 +10,7 @@
       >
         <el-menu-item index="MonitorList">在线考试列表</el-menu-item>
         <div class="flex-grow" />
-        <el-menu-item index="1">监考页面</el-menu-item>
+        <el-menu-item index="WindowsList">监考页面</el-menu-item>
       </el-menu>
     </template>
     <template #mainContent> <component :is="renderMap[activeIndex]"></component> </template>
@@ -19,18 +19,23 @@
 <script setup>
 import BlankCardWithOutBorder from "@/components/blankCardWithOutBorder.vue";
 import MonitorList from "@/components/invigilateManagement/monitorList.vue";
+import WindowsList from "@/components/invigilateManagement/windowsList.vue";
 import { ref } from "vue";
 const renderMap = {
   MonitorList: MonitorList,
+  WindowsList: WindowsList,
 };
 const activeIndex = ref("MonitorList");
 const handleSelect = (key) => {
-  console.log(key);
+  activeIndex.value = key;
 };
 </script>
 <style lang="less" scoped>
 .top-menu {
   border-top-left-radius: 8px;
   border-top-right-radius: 8px;
+}
+/deep/.el-menu--horizontal {
+  border-bottom: 0px;
 }
 </style>

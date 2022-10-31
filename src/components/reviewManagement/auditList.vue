4f3d7dd6f5372b3797501faacf0a3c6294dde719
魏加锴 <jkwei@foxmail.com>
2022-10-31 15:48:35 +0800

    qwer

--- src/components/reviewManagement/auditList.vue
+++ src/components/reviewManagement/auditList.vue
@@ -3,37 +3,26 @@
     <template #title>阅卷列表</template>
     <template #mainContent>
       <div class="flex-col flex audit-container mb-4">
-        <div
-          v-for="(item, index) in auditList"
-          class="flex items-center justify-between m-2 shadow-md p-4 audit-item"
-        >
+        <div v-for="(item, index) in auditList" class="flex items-center justify-between m-2 shadow-md p-4 audit-item">
           <!-- left -->
           <div class="relative flex">
             <img src="@/assets/image/u727.svg" alt="" />
             <p class="absolute top-0 left-0 mark">最新</p>
-            <img
-              src="@/assets/image/u728.svg"
-              alt=""
-              class="absolute top-0 left-0"
-            />
+            <img src="@/assets/image/u728.svg" alt="" class="absolute top-0 left-0" />
             <!-- 标题 -->
             <div class="flex flex-col ml-4 justify-between">
               <h3 style="font-size: 18px">{{ item.title }}</h3>
               <h5 class="item-h5">{{ item.time }}</h5>
               <template class="flex">
                 <span class="item-lable mr-2">应考人数</span>
-                <p class="m-auto" style="color: #31969a">
-                  {{ item.expectNumber }}人
-                </p>
+                <p class="m-auto" style="color: #31969a">{{ item.expectNumber }}人</p>
                 <span class="item-lable ml-4 mr-2">实考人数</span>
-                <p class="m-auto" style="color: red">
-                  {{ item.actualNumber }}人
-                </p>
+                <p class="m-auto" style="color: red">{{ item.actualNumber }}人</p>
               </template>
             </div>
           </div>
           <!-- right -->
-          <el-button type="primary">阅卷</el-button>
+          <el-button type="primary" style="border-radius: 16px" @click="startToReviewExam">阅卷</el-button>
         </div>
       </div>
     </template>
@@ -42,6 +31,12 @@
 <script setup>
 import { reactive } from "vue";
 import BasicCardVue from "@/components/basicCard.vue";
+import { useExamStore } from "@/store";
+const props = defineProps({
+  renderName: String|Object,
+});
+const emits = defineEmits();
+const examStore = useExamStore();
 const auditList = reactive([]);
 for (let index = 0; index < 20; index++) {
   auditList.push({
@@ -51,6 +46,10 @@ for (let index = 0; index < 20; index++) {
     actualNumber: 46,
   });
 }
+const startToReviewExam = () => {
+  emits("update:renderName", "ReviewList");
+  examStore.$reset();
+};
 </script>
 <style lang="less" scoped>
 @import url("@/assets/css/common.less");
--- src/views/reviewManagement/index.vue
+++ src/views/reviewManagement/index.vue
@@ -1,10 +1,17 @@
 <template>
   <div class="reviewManagement-container h-full">
-    <AuditList></AuditList>
+    <component :is="renderMap[renderName]" v-model:renderName="renderName"></component>
   </div>
 </template>
 <script setup>
+import { ref } from "vue";
 import AuditList from "@/components/reviewManagement/auditList.vue";
+import ReviewList from "@/components/reviewManagement/reviewList.vue";
+const renderName = ref("AuditList");
+const renderMap = {
+  AuditList: AuditList,
+  ReviewList: ReviewList,
+};
 </script>
 <style lang="less" scoped>
 .reviewManagement-container {
